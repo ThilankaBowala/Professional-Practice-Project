4af159336043880e10644be8f0c8b45e1aae6da6
Thilanka Bowala <thilankabowala@gmail.com>
2015-06-16 21:21:43 +0530

    First commit

---
+++ src/dahampasala/DahamPasala.java
@@ -0,0 +1,16 @@
+package dahampasala;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Thilanka Bowala <Thilanka Bowala at GIGABYTE>
+ */
+public class DahamPasala {
+
+    public static void main(String[] args) {
+        MainFrame mf = new MainFrame();
+        mf.setVisible(true);
+        mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
